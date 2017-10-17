b8266e9af3a5dd0c1daea6652534a82833e87c9b
Max <maksat.mansurov@gmail.com>
2017-10-17 10:33:05 +0600

    get info by id

--- lib/city_search.rb
+++ lib/city_search.rb
@@ -2,8 +2,23 @@ require 'dawg'
 module CitySearch
   extend self
   def search(q)
-    states = Marshal.load(File.read('../state-countries.bin'))
-    dawg = Dawg.load('../data/dawg.bin', :fast)
-    dawg.query(q)
+    results = dawg.query(q)
+    results.map do |r|
+      result = r.split(' ')
+      code = result.last
+      result.pop
+      result = result + states[code.to_i]
+    end
   end
+
+  private
+
+  def dawg
+    @@dawg ||= Dawg.load(File.dirname(__FILE__) + '/../data/dawg.bin', :fast)
+  end
+
+  def states
+    @@states ||= Marshal.load(File.read(File.dirname(__FILE__) + '/../data/state-countries.bin'))
+  end
+
 end
